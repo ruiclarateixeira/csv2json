1265a131c62a72557627da50517b65863d3689e6
Rui Teixeira <rui.clarateixeira@student.manchester.ac.uk>
2016-06-04 15:05:46 +0100

    Added functionality. Missing tests and checking the Effective GO guide

--- csv2json.go
+++ csv2json.go
@@ -1,10 +1,67 @@
 package csv2json
 
 import (
-	"fmt"
+	"encoding/csv"
+	"encoding/json"
+	"io"
+	"os"
 )
 
-func ReadFile(filePath string) string { // This will actually return a json array
-	fmt.Printf("Got: %s\n", filePath)
-	return filePath
+func ReadFile(filePath string) ([]byte, error) {
+	fileReader, err := os.Open(filePath)
+
+	if err != nil {
+		return make([]byte, 0), err
+	}
+
+	json, err := Read(fileReader)
+
+	if err != nil {
+		return make([]byte, 0), err
+	}
+
+	return json, nil
+}
+
+func Read(reader io.Reader) ([]byte, error) {
+	csvReader := csv.NewReader(reader)
+	entries := make([]map[string]string, 0)
+
+	headers, err := csvReader.Read()
+	if err != nil {
+		return make([]byte, 0), err
+	}
+
+	for {
+		row, err := ReadRow(csv.Reader(*csvReader), headers)
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return make([]byte, 0), err
+		}
+
+		entries = append(entries, row)
+	}
+
+	json, err := json.Marshal(entries)
+
+	return json, nil
+}
+
+func ReadRow(csvReader csv.Reader, headers []string) (map[string]string, error) {
+	row, err := csvReader.Read()
+	contents := make(map[string]string)
+
+	if err != nil {
+		return make(map[string]string, 0), err
+	}
+
+	for index, cell := range row {
+		contents[headers[index]] = cell
+	}
+
+	return contents, nil
 }
