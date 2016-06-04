7a68ab24089eae6b467874fc3e82b380eb8fd779
Rui Teixeira <rui.clarateixeira@student.manchester.ac.uk>
2016-06-04 11:46:17 +0100

    Initial commit - Adding sample go library

---
+++ csv2json.go
@@ -0,0 +1,10 @@
+package csv2json
+
+import(
+	"fmt"
+)
+
+func ReadFile(filePath string) string { // This will actually return a json array
+     fmt.Printf( "Got: %s\n", filePath );
+     return filePath;
+} 
